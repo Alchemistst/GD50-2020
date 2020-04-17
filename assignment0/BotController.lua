3c78da287abee367ee1fb4906140b2a526ed61c2
Alchemistst23 <javiermariagarciacuevas@gmail.com>
2020-04-17 17:33:26 +0200

    Added some comments

--- assignment0/BotController.lua
+++ assignment0/BotController.lua
@@ -14,9 +14,13 @@ function BotController:init(player, ball)
 end
 
 function BotController:control()
-    if (self.player.y + self.player.height * 2/3) < self.ball.y then
+    --This function moves the paddle to make sure the ball always stays within range of the paddle.
+    --If the ball is above the paddle it moves up, and if bellow, down.
+    --I reduced the range to activate the movement (those 2/3 and 1/3 coeficients) to make it a bit more
+    --acurate. Also, it's not just hitting it right on the edge of the paddle.
+    if (self.player.y + self.player.height*2/3) < (self.ball.y + self.ball.height) then
         self.player:moveDown()
-    elseif (self.player.y + self.player.height * 1/3) > (self.ball.y + self.ball.height) then
+    elseif (self.player.y + self.player.height*1/3) > self.ball.y then
         self.player:moveUp()
     else
         self.player:stop()
